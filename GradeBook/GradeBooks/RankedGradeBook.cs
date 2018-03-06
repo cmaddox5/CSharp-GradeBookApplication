806341ba10a44ce7bcf2de97d0e28deaf834e0bc
Christian Maddox <cmaddox5@vols.utk.edu>
2018-03-05 19:46:02 -0500

    Added GetLetterGrade override to RankedGradeBook
    Overrode the GetLetterGrade method in RankedGradeBook.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,34 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (averageGrade >= 80)
+            {
+                return 'A';
+            }
+            if (averageGrade >= 60)
+            {
+                return 'B';
+            }
+
+            if (averageGrade >= 40)
+            {
+                return 'C';
+            }
+
+            if (averageGrade >= 20)
+            {
+                return 'D';
+            }
+
+            return 'F';
+        }
     }
 }
